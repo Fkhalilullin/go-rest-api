dd9de73d824823aa2735ffa9b4bf6d3a3bd91fff
Naburi <58636313+Naburi@users.noreply.github.com>
2022-02-20 14:56:55 +0300

    Add updtate method

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -11,37 +12,76 @@ type Product struct {
 	ID          int    `json:"id"`
 	Price       int    `jsos:"price"`
 	Name        string `jsos:"name"`
-	Description string `jsos:"descrition"`
-	CreatedOn   string `jsos:"-"`
-	UpdatedOn   string `jsos:"-"`
-	DeletedOn   string `jsos:"-"`
+	Description string `jsos:"description"`
+	CreatedOn   string `jsos:"created"`
+	UpdatedOn   string `jsos:"updated"`
+	DeletedOn   string `jsos:"deleted"`
 }
 
 type Products []*Product
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
 
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
 
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
 func GetProducts() Products {
 	return productList
 }
 
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
 var productList = Products{
-	&Product{
+	{
 		ID:          1,
-		Price:       200,
-		Name:        "Latte",
-		Description: "Frothy milky coffee",
+		Price:       250,
+		Name:        "Eugene Onegin",
+		Description: "Novel in verse written by Alexander Pushkin",
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
-	&Product{
+	{
 		ID:          2,
 		Price:       300,
-		Name:        "Espresso",
-		Description: "Short and strong coffee without milk",
+		Name:        "War and Peace",
+		Description: "Literary work mixed with chapters on history and philosophy by the Russian author Leo Tolstoy",
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
